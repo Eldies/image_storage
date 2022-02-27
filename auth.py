2d80b83e2ee69aa6ab44b88da89b745a4739bd9f
Dmitry Lavrukhin <dlavrukhin@yandex.ru>
2022-02-27 11:12:51 +0300

    better errors from auth

--- auth.py
+++ auth.py
@@ -2,6 +2,19 @@
 import json
 
 
+class InvalidTokenError(ValueError):
+    pass
+
+
+class TokenDoesNotHaveIdError(InvalidTokenError):
+    pass
+
+
 def get_user_id_from_token(token):
-    decoded_token = json.loads(token)
-    return decoded_token['id']
+    try:
+        decoded_token = json.loads(token)
+        return decoded_token['id']
+    except KeyError:
+        raise TokenDoesNotHaveIdError()
+    except:
+        raise InvalidTokenError
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import unittest
-from json import JSONDecodeError
 
-from auth import get_user_id_from_token
+from auth import (
+    get_user_id_from_token,
+    InvalidTokenError,
+    TokenDoesNotHaveIdError,
+)
 
 
 class TestGetUserIdFromToken(unittest.TestCase):
@@ -10,9 +13,9 @@ class TestGetUserIdFromToken(unittest.TestCase):
         assert get_user_id_from_token('{"id": 4}') == 4
 
     def test_cant_decode_token(self):
-        with self.assertRaises(JSONDecodeError):
+        with self.assertRaises(InvalidTokenError):
             get_user_id_from_token('garbage')
 
     def test_token_dict_does_not_contain_id(self):
-        with self.assertRaises(KeyError):
+        with self.assertRaises(TokenDoesNotHaveIdError):
             get_user_id_from_token('{"foo": "bar"}')
