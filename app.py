10d5c34777c7174e2b38492a208050f241b73bc5
Dmitry Lavrukhin <dlavrukhin@yandex.ru>
2022-02-27 10:30:28 +0300

    logging configuration

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
+from logging.config import dictConfig
 
+import settings
 import views
 
 
-app = Flask(__name__)
+def create_app():
 
-app.add_url_rule('/ping', view_func=views.ping)
+    app = Flask(__name__)
+
+    app.add_url_rule('/ping', view_func=views.ping)
+
+    dictConfig(settings.LOGGING_CONFIG)
+
+    return app
+
+app = create_app()
---
+++ settings.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console']
+    },
+}
