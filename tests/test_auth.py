ce6222d5ea4131fff00cd8efa1f13f487cb0c57d
Dmitry Lavrukhin <dlavrukhin@yandex.ru>
2022-07-07 01:45:27 +0300

    removing unittest.TestCase

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-import unittest
 from unittest.mock import patch
 
 from app.auth import get_client_info_by_api_key
 from app.settings import ClientInfo
 
 
-class TestGetUserIdFromToken(unittest.TestCase):
+class TestGetUserIdFromToken:
     def test_ok(self):
         with patch('app.settings.CLIENTS_INFO', [ClientInfo(id='test_client', api_key='TEST_API_KEY')]):
             client = get_client_info_by_api_key('TEST_API_KEY')
