2bbe2dee4c27da757ed54d9217e6d9757cd6a478
Dmitry Lavrukhin <dlavrukhin@yandex.ru>
2022-07-14 15:16:09 +0300

    reorganizing tests

--- tests/views/test_images.py
+++ tests/views/test_images.py
@@ -6,23 +6,23 @@ import json
 import pytest
 
 
-class TestImageView:
+class TestImageViewPost:
     @pytest.fixture(autouse=True)
     def _setup(self, client, environment):
         self.client = client
         self.env = environment
 
-    def test_post_no_api_key(self):
+    def test_no_api_key(self):
         response = self.client.post('/v1/image/')
         assert response.status_code == 401
         assert response.json == {'status': 'error', 'error': 'Unauthorized'}
 
-    def test_post_wrong_api_key(self):
+    def test_wrong_api_key(self):
         response = self.client.post('/v1/image/', headers={'X-API-KEY': 'random_string'})
         assert response.status_code == 401
         assert response.json == {'status': 'error', 'error': 'Unauthorized'}
 
-    def test_post_no_file(self):
+    def test_no_file(self):
         response = self.client.post('/v1/image/', json={}, headers={'X-API-KEY': 'TEST_API_KEY'})
         assert response.status_code == 400
         assert response.json == {'status': 'error', 'error': 'No file'}
@@ -31,7 +31,7 @@ class TestImageView:
         'filename',
         None,
     ])
-    def test_post_ok(self, filename):
+    def test_ok(self, filename):
         data = dict()
         data['base64'] = base64.b64encode(b'abcdef').decode()
         if filename:
@@ -56,7 +56,14 @@ class TestImageView:
             mimetype='image/jpeg',
         )
 
-    def test_get_ok(self):
+
+class TestImageViewGet:
+    @pytest.fixture(autouse=True)
+    def _setup(self, client, environment):
+        self.client = client
+        self.env = environment
+
+    def test_ok(self):
         self.env.image_storage_mock.return_value.uuid_exists.return_value = True
         self.env.image_storage_mock.return_value.read_file.return_value = io.BytesIO(b'abcdef')
         self.env.image_storage_mock.return_value.read_data.return_value = '{"mimetype": "image/jpeg"}'
@@ -66,7 +73,7 @@ class TestImageView:
         assert response.data == b'abcdef'
         assert self.env.image_storage_mock.return_value.read_file.call_args.args[0] == ['some_uuid']
 
-    def test_get_ok_with_client_id(self):
+    def test_ok_with_client_id(self):
         self.env.image_storage_mock.return_value.uuid_exists.return_value = True
         self.env.image_storage_mock.return_value.read_file.return_value = io.BytesIO(b'abcdef')
         self.env.image_storage_mock.return_value.read_data.return_value = '{"mimetype": "image/jpeg"}'
@@ -76,6 +83,6 @@ class TestImageView:
         assert response.data == b'abcdef'
         assert self.env.image_storage_mock.return_value.read_file.call_args.args[0] == ['some_client_id', 'some_uuid']
 
-    def test_get_unknown_uuid(self):
+    def test_unknown_uuid(self):
         response = self.client.get('/v1/image/some_uuid')
         assert response.status_code == 404
