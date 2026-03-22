0fa525e5fda404e01994e9a27d6b864e98f81893
Dmitrii Lavrukhin <lavrukhindmitry@gmail.com>
2026-03-22 23:35:15 +0300

    fix isort

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from typing import Optional, Literal
+from typing import Literal, Optional
 
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
